ba263acc6e3a0a59db2fc0fe6cdf59f7ab43e0fa
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-17 16:21:04 +0100

    pushed

--- syntax/characterAccess/GetLastCharacter.js
+++ syntax/characterAccess/GetLastCharacter.js
@@ -1,3 +1,7 @@
 function getLastCharacter(name){
     return name[name.length - 1]
 }
+
+console.log(getLastCharacter("Kunle"))
+console.log(getLastCharacter("Emmanuel"))
+console.log(getLastCharacter("Bukky"))
