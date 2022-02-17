7df41ec09616e115dfa299e03a5181651d5b259f
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-17 13:55:37 +0100

    shout my name

---
+++ syntax/exercise/GetCharCount.js
@@ -0,0 +1,7 @@
+function getCharCount(str){
+    return str.length;
+}
+
+console.log(getCharCount("sam"))
+console.log(getCharCount("Alex 123"))
+console.log(getCharCount("Charley is here."))
---
+++ syntax/exercise/ShoutMyName.js
@@ -0,0 +1,7 @@
+function shoutMyName(name){
+    return name.toUpperCase()
+}
+
+console.log(shoutMyName("Sam"))
+console.log(shoutMyName("Charley"))
+console.log(shoutMyName("Alex"))
