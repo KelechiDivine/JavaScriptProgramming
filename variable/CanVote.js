4b1369b1de76a50a3f98e198c45b284e614f7e3a
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-18 13:02:37 +0100

    done

---
+++ variable/CanVote.js
@@ -0,0 +1,3 @@
+function canVote(age){ return age >= 18}
+
+console.log(canVote(23))
---
+++ variable/OddAndEven.js
@@ -0,0 +1,9 @@
+function oddAndEven(number){
+    if (number % 2 === 0){
+        return "even"
+    }
+    return "odd"
+}
+
+console.log(oddAndEven(32))
+console.log(oddAndEven(21))
