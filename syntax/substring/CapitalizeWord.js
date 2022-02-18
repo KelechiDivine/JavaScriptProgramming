ed3d0bc89b0642006856fb7ad620fb275fcea1e6
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-18 10:39:17 +0100

    number convertion

---
+++ syntax/substring/CapitalizeWord.js
@@ -0,0 +1,7 @@
+function capitalizeWord(word){
+    return word[0].toUpperCase() + word.substring(1).toLowerCase()
+}
+
+console.log(capitalizeWord("sam"))
+console.log(capitalizeWord("ALEX"))
+console.log(capitalizeWord("chARLie"))
